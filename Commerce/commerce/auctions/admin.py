ae295795a8386abeed8a1c4de616d75268cd7132
leohx2 <leobgse@gmail.com>
2023-02-17 18:00:23 +0000

    Creating adm super user

--- Commerce/commerce/auctions/admin.py
+++ Commerce/commerce/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Bids, Comments, Listings, User, WatchList
+
 # Register your models here.
+admin.site.register(Bids)
+admin.site.register(Comments)
+admin.site.register(Listings)
+admin.site.register(User)
+admin.site.register(WatchList)
