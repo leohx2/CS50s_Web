3a098541effef240a0ec7b8dfe760cdc91eb40f1
leohx2 <leobgse@gmail.com>
2023-09-06 19:12:11 +0100

    Changing transiction

--- FinalProject/finalproject/portfolio/static/portfolio/javaScript/transiction.js
+++ FinalProject/finalproject/portfolio/static/portfolio/javaScript/transiction.js
@@ -1,14 +1,14 @@
 
 // Using that file will allow me to focus on the transictions of the web site
-const transition = document.querySelector('.transictionMaker');
+const transition = document.querySelector('body');
 
 export function transictionMaker (func) {
     // Add the transiction effect and wait 
-    transition.classList.add("transition");
+    transition.classList.add("transitionOpacity");
 
     // Set timeOut to run the function only when the transition is on 50% of the duration.
-    setTimeout(() => func(), 1500);
+    setTimeout(() => func(), 500);
 
     // Set timeOut to remove the transition class after the transition is over
-    setTimeout(() => transition.classList.remove("transition"), 2500);
+    setTimeout(() => transition.classList.remove("transitionOpacity"), 1000);
 }
